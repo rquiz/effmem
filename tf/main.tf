e85dff871a80607ce6448fc637daf426dd1e1753
Ryan Quizenberry <974958+rquiz@users.noreply.github.com>
2023-09-28 12:38:26 -0700

    Use a variable in TF to verify passing correctly

--- tf/main.tf
+++ tf/main.tf
@@ -58,7 +58,7 @@ resource "kubernetes_deployment" "test" {
 # K8s services manage networking and routing to deployments
 resource "kubernetes_service" "test" {
   metadata {
-    name      = "nginx"
+    name      = "mysql_username"
     namespace = kubernetes_namespace.test.metadata.0.name
   }
   spec {
